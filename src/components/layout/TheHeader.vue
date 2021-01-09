bbff260f722d2c7d0769ef72134acaa93c7bde2e
Alex <csiszaralex@gmail.com>
2021-01-09 12:47:40 +0100

    :art: Add test buttons to header

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -13,6 +13,8 @@
         Quizy
       </router-link>
       <div class="d-flex mr-n">
+        <base-button to="/asd" type="warning">NemLétező</base-button>
+        <base-button to="/teacher" type="danger">Tanár</base-button>
         <base-button to="/auth?mode=reg" type="info">Regisztráció</base-button>
         <base-button to="/auth?mode=login" type="success">Bejelentkezés</base-button>
       </div>
@@ -20,9 +22,10 @@
   </nav>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .navbar {
-  padding: 0;
+  padding-top: 0;
+  padding-bottom: 0;
   margin-right: 0;
   overflow-x: hidden;
   .mr-n {
