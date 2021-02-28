3fc95b1c7aca005f1d73e0fc6dde9250170a2af1
Alex <csiszaralex@gmail.com>
2021-02-28 11:54:04 +0100

    :construction: Start quiz editor

---
+++ src/components/edit/editPreview.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="row mx-3 my-2 qSet py-3 d-flex justify-content-center" :style="{ order: srsz }">
+    {{ srsz }} - {{ nev }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditPreview',
+  props: ['srsz', 'nev'],
+};
+</script>
--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,24 +1,49 @@
 <template>
-  <div class="container-fluid flex-grow-1 d-flex">
+  <div class="container-fluid flex-grow-1 d-flex p-0">
     <base-dialog :show="!!uzenet" title="Folyamatban" @close="elfogad">
       <p>{{ uzenet }}</p>
     </base-dialog>
-    <div class="col-3 flex-grow-1 d-flex flex-column">
-      <div class="h-75">Diák</div>
-      <div class="h-25 text-center py-2 qSet">
-        <button @click="wip">Import</button>
-        <p>Export</p>
-        <p>Beállítások</p>
+    <div class="col-3 flex-grow-1 d-flex flex-column bg-light">
+      <div class="flex-grow-1 d-flex flex-column">
+        <edit-preview
+          v-for="item in data.questions"
+          :key="item"
+          :srsz="item.srsz"
+          :nev="item.name"
+        ></edit-preview>
+        <edit-preview :srsz="99" nev="PLUSZ"></edit-preview>
+      </div>
+      <div class="text-center py-2 qSet">
+        <p class="btn d-block p-0 gomb mb-2" @click="wip">Importálás</p>
+        <p class="btn d-block p-0 gomb mb-2" @click="wip">Exportálás</p>
+        <p class="btn d-block p-0 gomb mb-2">Beállítások</p>
       </div>
     </div>
-    <div class="col-9 flex-grow-1 bg-warning">Ediiit {{ id }}</div>
+    <div class="col-9 flex-grow-1">Ediiit {{ id }}</div>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+import { useStore } from 'vuex';
+import teacher from '@/config/axiosTeacher.config';
+import editPreview from '../components/edit/editPreview.vue';
+
 export default {
   name: 'Edit',
+  components: { editPreview },
   props: ['id'],
+  setup(props) {
+    const store = useStore();
+    const id = store.getters.getId;
+
+    const data = ref({});
+    teacher.get(`/${id}/${props.id}.json`).then(res => {
+      data.value = res.data;
+    });
+
+    return { data };
+  },
   data() {
     return { uzenet: null };
   },
@@ -32,3 +57,9 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.gomb {
+  font-size: 1.1rem;
+}
+</style>
